751bc27d71a4dc7abd8ddc5428f232ec9200175b
Isabel Lamim <lamim@protonmail.com>
2022-08-17 16:30:32 +0200

    a couple more steps in building simple airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,14 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def show
+    set_flat
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 end
